254ada5f6932efde4b985ffa0f2f7fdcbccc7106
Adam Neulight <arneulight@gmail.com>
2024-10-30 23:02:59 -0400

    tidying up

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #define BR 7
 #define JOY_V_PIN A0
 #define JOY_H_PIN A1
+#define SCROLL_SPEED_DELAY 150
 
 
 // put function declarations here:
@@ -18,6 +19,7 @@ void buttonActions();
 void scrollActions();
 
 int buttons[8];
+int lastButtons[4] = {1,1,1,1};
 int scrolls[2];
 unsigned long nextMillisH = 0;
 unsigned long nextMillisV = 0;
@@ -63,42 +65,62 @@ void buttonActions(){
   if(!buttons[1]){Mouse.press(MOUSE_RIGHT);}
   else{Mouse.release(MOUSE_RIGHT);}
 
-  if(!buttons[2]){
+  if(!buttons[2] && lastButtons[0]){ //  if button 2 is pressed, and its last state was unpressed
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.write('s');
+    Keyboard.press('s');
     Keyboard.releaseAll();
+    lastButtons[0] = 0;
+  }
+  else if(buttons[2]){ // if button is unpressed, reset lastButtons
+    lastButtons[0] = 1;
   }
 
-  if(!buttons[3]){
+  if(!buttons[3] && lastButtons[1]){ //  if button 2 is pressed, and its last state was unpressed
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.write('c');
+    Keyboard.press('c');
     Keyboard.releaseAll();
+    lastButtons[1] = 0;
+  }
+  else if(buttons[3]){ // if button is unpressed, reset lastButtons
+    lastButtons[1] = 1;
   }
 
-  if(!buttons[4]){
+  if(!buttons[4] && lastButtons[2]){ //  if button 2 is pressed, and its last state was unpressed
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.write('x');
+    Keyboard.press('x');
     Keyboard.releaseAll();
+    lastButtons[2] = 0;
+  }
+  else if(buttons[4]){ // if button is unpressed, reset lastButtons
+    lastButtons[2] = 1;
   }
   
-  if(!buttons[5]){
+  if(!buttons[5] && lastButtons[3]){ //  if button 2 is pressed, and its last state was unpressed
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.write('v');
+    Keyboard.press('v');
     Keyboard.releaseAll();
+    lastButtons[3] = 0;
+  }
+  else if(buttons[5]){ // if button is unpressed, reset lastButtons
+    lastButtons[3] = 1;
   }
 }
 
 void scrollActions(){
   if(scrolls[0] < 400){
     if(nextMillisV < millis()){
+      delay(1);
       Mouse.move(0, 0, -1);
-      nextMillisV = millis() + 150;
+      delay(1);
+      nextMillisV = millis() + SCROLL_SPEED_DELAY;
     }
   }
   else if(scrolls[0] > 625){
     if(nextMillisV < millis()){
+      delay(1);
       Mouse.move(0, 0, 1);
-      nextMillisV = millis() + 150;
+      delay(1);
+      nextMillisV = millis() + SCROLL_SPEED_DELAY;
     }
   }
   
@@ -109,7 +131,7 @@ void scrollActions(){
       Mouse.move(0, 0, 1);
       delay(1);
       Keyboard.release(KEY_LEFT_SHIFT);
-      nextMillisH = millis() + 150;
+      nextMillisH = millis() + SCROLL_SPEED_DELAY;
     }
   }
   else if(scrolls[1] > 625){
