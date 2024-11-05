21c56796c8aa28b23a91740d4d1778fa7de94e0a
Adam Neulight <arneulight@gmail.com>
2024-11-05 10:55:05 -0500

    added some config options, will add more later

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include <Arduino.h>
-#include <HID-Project.h>
+#include <Mouse.h>
+#include <Keyboard.h>
 
-#define TL 2
-#define TM 3
-#define TR 4
-#define BL 5
-#define BM 6
-#define BR 7
+#define TopLeft 2
+#define TopMiddle 3
+#define TopRight 4
+#define LowerLeft 5
+#define LowerMiddle 6
+#define LowerRight 7
 #define JOY_V_PIN A0
 #define JOY_H_PIN A1
-#define SCROLL_SPEED_DELAY 150
+
+#define MOUSECLICK1 MOUSE_LEFT
+#define MOUSECLICK2 MOUSE_RIGHT
+
+#define BUTTON1 'c'
+#define BUTTON2 'z'
+#define BUTTON3 'v'
+#define BUTTON4 's'
+
+#define BUTTON_DEBUG 0
+
+#define SCROLL_SPEED_DELAY 300.0
+#define MIN_SCROLL_SPEED_DELAY 30
 
 
 // put function declarations here:
@@ -28,13 +41,15 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(JOY_H_PIN, INPUT);
   pinMode(JOY_V_PIN, INPUT);
-  pinMode(TL, INPUT_PULLUP);
-  pinMode(TM, INPUT_PULLUP);
-  pinMode(TR, INPUT_PULLUP);
-  pinMode(BL, INPUT_PULLUP);
-  pinMode(BM, INPUT_PULLUP);
-  pinMode(BR, INPUT_PULLUP);
-  //Serial.begin(9600);
+  pinMode(TopLeft, INPUT_PULLUP);
+  pinMode(TopMiddle, INPUT_PULLUP);
+  pinMode(TopRight, INPUT_PULLUP);
+  pinMode(LowerLeft, INPUT_PULLUP);
+  pinMode(LowerMiddle, INPUT_PULLUP);
+  pinMode(LowerRight, INPUT_PULLUP);
+  if(BUTTON_DEBUG){
+      Serial.begin(9600);
+  }
   Mouse.begin();
   Keyboard.begin();
 }
@@ -59,16 +74,29 @@ void pollButtons(){
 }
 
 void buttonActions(){
-  if(!buttons[0]){Mouse.press(MOUSE_LEFT);}
-  else{Mouse.release(MOUSE_LEFT);}
+  if(!buttons[0]){
+    Mouse.press(MOUSECLICK1);
+    if(BUTTON_DEBUG){
+      Serial.println("MOUSE1");
+    }
+  }
+  else{Mouse.release(MOUSECLICK1);}
 
-  if(!buttons[1]){Mouse.press(MOUSE_RIGHT);}
-  else{Mouse.release(MOUSE_RIGHT);}
+  if(!buttons[1]){
+    Mouse.press(MOUSECLICK2);
+    if(BUTTON_DEBUG){
+      Serial.println("MOUSE2");
+    }
+  }
+  else{Mouse.release(MOUSECLICK2);}
 
   if(!buttons[2] && lastButtons[0]){ //  if button 2 is pressed, and its last state was unpressed
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.press('s');
+    Keyboard.press(BUTTON1);
     Keyboard.releaseAll();
+    if(BUTTON_DEBUG){
+      Serial.println("BUTTON1");
+    }
     lastButtons[0] = 0;
   }
   else if(buttons[2]){ // if button is unpressed, reset lastButtons
@@ -77,8 +105,11 @@ void buttonActions(){
 
   if(!buttons[3] && lastButtons[1]){ //  if button 2 is pressed, and its last state was unpressed
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.press('c');
-    Keyboard.releaseAll();
+    Keyboard.press(BUTTON2);
+    Keyboard.releaseAll();    
+    if(BUTTON_DEBUG){
+      Serial.println("BUTTON2");
+    }
     lastButtons[1] = 0;
   }
   else if(buttons[3]){ // if button is unpressed, reset lastButtons
@@ -87,8 +118,11 @@ void buttonActions(){
 
   if(!buttons[4] && lastButtons[2]){ //  if button 2 is pressed, and its last state was unpressed
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.press('x');
+    Keyboard.press(BUTTON3);
     Keyboard.releaseAll();
+    if(BUTTON_DEBUG){
+      Serial.println("BUTTON3");
+    }
     lastButtons[2] = 0;
   }
   else if(buttons[4]){ // if button is unpressed, reset lastButtons
@@ -97,8 +131,11 @@ void buttonActions(){
   
   if(!buttons[5] && lastButtons[3]){ //  if button 2 is pressed, and its last state was unpressed
     Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.press('v');
+    Keyboard.press(BUTTON4);
     Keyboard.releaseAll();
+    if(BUTTON_DEBUG){
+      Serial.println("BUTTON4");
+    }
     lastButtons[3] = 0;
   }
   else if(buttons[5]){ // if button is unpressed, reset lastButtons
@@ -107,41 +144,33 @@ void buttonActions(){
 }
 
 void scrollActions(){
-  if(scrolls[0] < 400){
-    if(nextMillisV < millis()){
+  if(scrolls[0] < 475 && nextMillisV < millis()){ // Vertical potentiometer
       delay(1);
       Mouse.move(0, 0, -1);
       delay(1);
-      nextMillisV = millis() + SCROLL_SPEED_DELAY;
-    }
+      nextMillisV = millis() + MIN_SCROLL_SPEED_DELAY + (int)(SCROLL_SPEED_DELAY * ((float)scrolls[0] / 475.0));
   }
-  else if(scrolls[0] > 625){
-    if(nextMillisV < millis()){
+  else if(scrolls[0] > 650 && nextMillisV < millis()){
       delay(1);
       Mouse.move(0, 0, 1);
       delay(1);
-      nextMillisV = millis() + SCROLL_SPEED_DELAY;
-    }
+      nextMillisV = millis() + MIN_SCROLL_SPEED_DELAY + (int)(SCROLL_SPEED_DELAY * ((1024.0 - (float)scrolls[0]) / 374.0));
   }
   
-  if(scrolls[1] < 400){
-    if(nextMillisH < millis()){
+  if(scrolls[1] < 475 && nextMillisH < millis()){ // Horizontal potentiometer
       Keyboard.press(KEY_LEFT_SHIFT);
       delay(1);
       Mouse.move(0, 0, 1);
       delay(1);
       Keyboard.release(KEY_LEFT_SHIFT);
-      nextMillisH = millis() + SCROLL_SPEED_DELAY;
-    }
+      nextMillisH = millis() + MIN_SCROLL_SPEED_DELAY + (int)(SCROLL_SPEED_DELAY * ((float)scrolls[1] / 475.0));
   }
-  else if(scrolls[1] > 625){
-    if(nextMillisH < millis()){
+  else if(scrolls[1] > 650 && nextMillisH < millis()){
       Keyboard.press(KEY_LEFT_SHIFT);
       delay(1);
       Mouse.move(0, 0, -1);
       delay(1);
       Keyboard.release(KEY_LEFT_SHIFT);
-      nextMillisH = millis() + 150;
-    }
+      nextMillisH = millis() + MIN_SCROLL_SPEED_DELAY + (int)(SCROLL_SPEED_DELAY * ((1024.0 - (float)scrolls[1]) / 374.0));
   }
 }
